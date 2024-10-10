a2b1c784220347a423f1a9c343f91bbc2fa542c3
arpithaP <163737012+arpithaBosch@users.noreply.github.com>
2024-10-10 08:27:20 +0530

    Update colorData.js

--- app/colorData.js
+++ app/colorData.js
@@ -1,11 +1,17 @@
-export const MajorColorNames = ["WHITE", "RED", "BLACK", "YELLOW", "VIOLET"];
-export const MinorColorNames = ["BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"];
+const MajorColorNames = ["WHITE", "RED", "BLACK", "YELLOW", "VIOLET"];
+const MinorColorNames = ["BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"];
 
-export function ColorPair() {
+function ColorPair() {
     this.majorColor;
     this.minorColor;
 }
 
 ColorPair.prototype.toString = function() {
     return `MajorColor: ${this.majorColor}, MinorColor: ${this.minorColor}`;
+};
+
+module.exports = {
+    MajorColorNames,
+    MinorColorNames,
+    ColorPair
 };
